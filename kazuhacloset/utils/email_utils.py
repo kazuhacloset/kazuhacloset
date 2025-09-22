515dced4d1f1aeae4984b824923cf3a682dcc3e1
sahilshukla302003 <148991684+sahilshukla302003@users.noreply.github.com>
2025-09-22 18:38:18 +0530

    brevo implmented

--- kazuhacloset/utils/email_utils.py
+++ kazuhacloset/utils/email_utils.py
@@ -1,17 +1,29 @@
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from threading import Thread
 import os
+from threading import Thread
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
 
-def send_email_async(to_email, subject, html_content, plain_text_content):
+def send_email_async(to_email, subject, html_content, plain_text_content=""):
     def send():
-        sg = SendGridAPIClient(api_key=os.getenv("SENDGRID_API_KEY"))
-        message = Mail(
-            from_email=os.getenv("SENDGRID_FROM_EMAIL"),
-            to_emails=to_email,
+        configuration = sib_api_v3_sdk.Configuration()
+        configuration.api_key['api-key'] = os.getenv("BREVO_API_KEY")
+
+        api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+
+        sender = {"name": "Kazuha Closet", "email": os.getenv("BREVO_FROM_EMAIL")}
+        to = [{"email": to_email}]
+
+        send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
+            to=to,
+            sender=sender,
             subject=subject,
-            plain_text_content=plain_text_content,
-            html_content=html_content
+            html_content=html_content,
+            text_content=plain_text_content
         )
-        sg.send(message)
+
+        try:
+            api_instance.send_transac_email(send_smtp_email)
+        except ApiException as e:
+            print(f"Exception when sending email: {e}")
+
     Thread(target=send, daemon=True).start()
